381443b1a30099da76d65b78ac78ccd6de57d288
opilane <opilane@HP-CZC2349HTY>
2024-01-10 13:28:15 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Ankeet()
+        {
+            return View();
+        }
+
+        public ActionResult Kutse()
+        {
+            ViewBag.Greeting = DateTime.Now.Hour<10 ? "Tere hommikust" : "Tere paevast";
+            ViewBag.Message = "Ootan sind!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Guest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Guest
+    {
+        [Required(ErrorMessage ="Sisesta nimi siia")]
+        public int Name { get; set; }
+
+        [Required(ErrorMessage = "Sisesta aastat vana siia")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Sisesta epost siia")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage ="Vale postkast")]
+        public string Email { get; set; }
+        [RegularExpression(@".+\+372[\d]{8}", ErrorMessage = "Vale number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Sisesta oma valik siia")]
+        public bool WillAttend { get; set; }
+
+
+    }
+}
